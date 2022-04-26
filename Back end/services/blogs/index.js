0d2942b7932f629c694deb58230df738352d126a
giorgio <giorgiolink94@hotmail.it>
2022-04-26 16:16:35 +0100

    CRUD finished

--- Back end/services/blogs/index.js
+++ Back end/services/blogs/index.js
@@ -18,30 +18,41 @@ blogRouter.post("/", async function (req, res, next) {
 
 blogRouter.get("/", async function (req, res, next) {
   try {
+    const blogs = await blogSchema.find();
+    res.send(blogs);
   } catch (error) {
     next(error);
   }
 });
-//*********************************************************GET
+//*********************************************************GET BY ID
 
 blogRouter.get("/:userid", async function (req, res, next) {
   try {
+    const selectedBlog = await blogSchema.findById(req.params.userid);
+    res.send(selectedBlog);
+  } catch (error) {
+    next(createError(404, "blog not found"));
+  }
+});
+//*********************************************************UPDATE
+
+blogRouter.put("/:userid", async function (req, res, next) {
+  try {
+    const updatedBlog = await blogSchema.findOneAndUpdate(
+      req.params.userid,
+      req.body
+    );
+    res.send(updatedBlog);
   } catch (error) {
     next(error);
   }
 });
-//*********************************************************GET
+//d*********************************************************DELETE
 
-blogRouter.get("/:userid", async function (req, res, next) {
+blogRouter.delete("/:userid", async function (req, res, next) {
   try {
-  } catch (error) {
-    next(error);
-  }
-});
-//d*********************************************************GET
-
-blogRouter.get("/:userid", async function (req, res, next) {
-  try {
+    const deleteBlog = await blogSchema.findOneAndDelete(req.params.userid);
+    res.status(204).send("Blog deleted");
   } catch (error) {
     next(error);
   }
