5a284fb7c1afbc72e740a6752c850cb6107df2af
giorgio <giorgiolink94@hotmail.it>
2022-04-26 15:20:32 +0100

    Update on endpoints

---
+++ Back end/services/blogs/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+
+const blogRouter = express.Router();
+
+//*********************************************************GET
+
+blogRouter.get("/", async function (req, res, next) {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+//*********************************************************GET
+
+blogRouter.get("/", async function (req, res, next) {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+//*********************************************************GET
+
+blogRouter.get("/:userid", async function (req, res, next) {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+//*********************************************************GET
+
+blogRouter.get("/:userid", async function (req, res, next) {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+//d*********************************************************GET
+
+blogRouter.get("/:userid", async function (req, res, next) {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
