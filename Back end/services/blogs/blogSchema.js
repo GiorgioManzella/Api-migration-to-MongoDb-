b72ea325048e8858e4d533034dea901ef04e522e
giorgio <giorgiolink94@hotmail.it>
2022-05-24 14:58:53 +0100

    Update

--- Back end/services/blogs/blogSchema.js
+++ Back end/services/blogs/blogSchema.js
@@ -1,20 +1,35 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcrypt";
 const { Schema, model } = mongoose;
 
 const blogSchema = new Schema(
   {
-    category: { type: String, required: true },
-    title: { type: String, required: true },
-    cover: { type: String, required: true },
+    category: { type: String },
+    title: { type: String },
+    cover: { type: String },
     readTime: {
-      value: { type: Number, required: true },
-      avatar: { type: String, required: true },
+      value: { type: Number },
+      avatar: { type: String },
     },
-    content: { type: String, required: true },
+    content: { type: String },
     comments: [{ title: { type: String }, comment: { type: String } }],
+    userName: { type: String },
+    email: { type: String },
+    password: { type: String },
   },
   { timestamps: true }
 );
+
+blogSchema.pre("save", async function (next) {
+  const Blog = this;
+  const password = this.password;
+
+  if (Blog.isModified()) {
+    const hash = await bcrypt.hash(password, 10);
+    Blog.password = hash;
+    next();
+  } else {
+  }
+});
 
 export default model("Blog", blogSchema); // linked to user collection / if not present will be created
